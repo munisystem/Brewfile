26bacf23b8e5aeebe9bf8df05cf7ee8f09e86996
Yuichi Saito <munisystem@gmail.com>
2017-03-09 10:52:23 +0900

    Fix unmove script

--- backup-brew.sh
+++ backup-brew.sh
@@ -6,7 +6,7 @@ set -u
 repository=$HOME/Brewfile
 
 cd "$repository"
-/usr/local/bin/brew brewdle dump -f
+/usr/local/bin/brew bundle dump -f
 /usr/bin/git add .
 /usr/bin/git commit -m "Backup Brewfile `date "+%Y-%m-%d %H:%M:%S"`"
 /usr/bin/git push origin master
